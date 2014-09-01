e360655687d4942c18f9b5aedd972531991ee699
Simon DELAGE <simon.geekitude@gmail.com>
2014-09-01 15:37:49 +0200

    Removed patern from [templatepath] option
    Was preventing to change the path

--- conf/metadata.php
+++ conf/metadata.php
@@ -8,7 +8,7 @@ $meta['use_name_string'] = array('onoff');      // use name string instead of lo
 $meta['use_start_page'] = array('onoff');      // use start page instead of namespace string for home page
 $meta['set_permissions'] = array('onoff');      // configure permissions
 $meta['set_permissions_others'] = array('multichoice','_choices'=>array('0','1','2','4','8','16')); // if enable set permission, what permission for other people?
-$meta['templatepath'] = array('string','_pattern' => '/^(|[a-zA-Z\-]+)$/'); // the location of the template file
+$meta['templatepath'] = array('string'); // the location of the template file
 $meta['users_namespace'] = array('string','_pattern' => '/^(|[a-zA-Z\-:]+)$/'); // the namespace containing user directories 
 $meta['group_by_name'] = array('onoff');      // group people by the first character of name
 $meta['edit_before_create'] = array('onoff');   // allow users to edit their home page before create.
