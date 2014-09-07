45bfa03d22d5f6a336c2e180c734b57bb0bc5506
Simon DELAGE <simon.geekitude@gmail.com>
2014-09-07 12:08:01 +0200

    Changed some options' text

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -6,7 +6,7 @@
  * @license: CC Attribution-Share Alike 3.0 Unported <http://creativecommons.org/licenses/by-sa/3.0/>
  */
 
-    $lang['create_private_ns'] = 'Create user\'s private namespace';
+    $lang['create_private_ns'] = 'Create user\'s private namespace (double-check all options before enabling)';
     $lang['use_name_string'] = 'Use user\'s full name instead of login for his private namespace (enabling this option will prevent using <code>%USER%</code> ACL wildcard and make ACL more complex).';
     $lang['use_start_page'] = 'Use the wiki\'s start page string instead of the user\'s namespace string set above for the start page of his namespace.';
     $lang['users_namespace'] = 'Namespace under which user namespaces are created (can be anything but <code>user</code> without messing up with Dokuwiki default behaviour).';
@@ -20,8 +20,8 @@
     $lang['set_permissions_others_o_16'] = 'Delete';
     $lang['group_by_name'] = 'Group users\' namespaces by the first character of user name?';
     $lang['edit_before_create'] = 'Allow users to edit their namespace start page before create (this will only work if you disable [create_public_page]).';
-    $lang['create_public_page'] = 'Create a public page for user';
+    $lang['create_public_page'] = 'Create a public page for user (double-check all options before enabling)';
     $lang['public_pages_ns'] = 'Namespace under wich public pages are created. If a value other than <code>user</code> is used, you will have to manually handle interwiki user link used by <code>showuseras</code> option (in Display settings).';
     $lang['set_permissions_public'] = 'Automatically configure ACL for the namespace set above (read access to anyone and edit rigth for each user on his own public plage).';
     $lang['templates_path'] = 'Path from DokuWiki root to store templates (userhomepage_private.txt and userhomepage_public.txt). Examples: <code>data/pages/wiki</code> (wich will make templates editable within DokuWiki) or <code>lib/plugins/userhomepage</code> (usefull in a farm setup to centralize templates).';
-    $lang['templatepath'] = 'Template path from version 3.0.4 if it was installed before. Don\'t change it, it is only there for backward compatibility. If this file exists, it will be used as default for new private namespace start page template.';
+    $lang['templatepath'] = 'Template path from version 3.0.4 if it was installed before. This option is only there for backward compatibility. If this file exists, it will be used as default for new private namespace start page template (clear the path if you don\'t want to).';
