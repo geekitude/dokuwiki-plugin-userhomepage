e470c482b926a8d14da9abb45b3d07b8aa715001
Hideaki SAWADA <chuno@live.jp>
2014-09-20 19:45:15 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,13 @@
+<?php
+ 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['copyerror'] = 'コピー失敗';
+$lang['copysuccess'] = 'コピー成功';
+$lang['createdprivatens'] = '私用の名前空間を作成した。';
+$lang['createdpublicpage'] = '公開ページを作成した。';
+$lang['privatenamespace'] = '私用の名前空間';
+$lang['publicpage'] = '公開ページ';
