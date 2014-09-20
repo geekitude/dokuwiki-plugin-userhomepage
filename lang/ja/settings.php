1e33c875d4953ef9ba5c418d58bfe54b396fc445
Hideaki SAWADA <chuno@live.jp>
2014-09-20 19:48:30 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,53 @@
+<?php
+ 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['create_private_ns'] = 'ユーザーの私用の名前空間の作成（有効化する前に全オプションを再度確認して下さい）';
+$lang['use_name_string'] = '私用の名前空間名としてユーザー名の代わりに氏名を使用する。';
+$lang['use_start_page'] = 'Wiki のスタートページ名を私用の名前空間のスタートページに使用する（無効の場合、名前空間名を使用）。';
+$lang['users_namespace'] = 'ユーザーの名前空間を作成する名前空間';
+// $lang['group_by_name'] = 'Group users\' namespaces by the first character of user name?';
+$lang['edit_before_create'] = '私用の名前空間作成時にスタートページの編集を許可する（同時に公開ページを作成しない場合のみ）。';
+$lang['acl_all_private'] = '私用の名前空間に対する @ALL グループのアクセス権限';
+$lang['acl_all_private_o_0'] = '無し（デフォルト）';
+$lang['acl_all_private_o_1'] = '読取';
+$lang['acl_all_private_o_2'] = '編集';
+$lang['acl_all_private_o_4'] = '作成';
+$lang['acl_all_private_o_8'] = 'アップロード';
+$lang['acl_all_private_o_16'] = '削除';
+// $lang['acl_all_private_o_noacl'] = 'No automatic ACL';
+$lang['acl_user_private'] = '私用の名前空間に対する @user グループのアクセス権限';
+$lang['acl_user_private_o_0'] = '無し（デフォルト）';
+$lang['acl_user_private_o_1'] = '読取';
+$lang['acl_user_private_o_2'] = '編集';
+$lang['acl_user_private_o_4'] = '作成';
+$lang['acl_user_private_o_8'] = 'アップロード';
+$lang['acl_user_private_o_16'] = '削除';
+// $lang['acl_user_private_o_noacl'] = 'No automatic ACL';
+$lang['create_public_page'] = 'ユーザーの公開ページの作成';
+$lang['public_pages_ns'] = '公開ページを作成する名前空間';
+$lang['acl_all_public'] = '公開ページに対する @ALL グループのアクセス権限';
+$lang['acl_all_public_o_0'] = '無し';
+$lang['acl_all_public_o_1'] = '読取（デフォルト）';
+$lang['acl_all_public_o_2'] = '編集';
+// $lang['acl_all_public_o_noacl'] = 'No automatic ACL';
+$lang['acl_user_public'] = '公開ページに対する @user グループのアクセス権限';
+$lang['acl_user_public_o_0'] = '無し';
+$lang['acl_user_public_o_1'] = '読取（デフォルト）';
+$lang['acl_user_public_o_2'] = '編集';
+// $lang['acl_user_public_o_noacl'] = 'No automatic ACL';
+$lang['templates_path'] = 'テンプレート（userhomepage_private.txt と userhomepage_public.txt）を置くパス。例：<code>data/pages/user</code> または <code>lib/plugins/userhomepage</code>';
+$lang['templatepath'] = 'バージョン 3.0.4 から引き継いだテンプレートのパス。このファイルがある場合、私用の名前空間の新規スタートページテンプレートのデフォルト元として使用する（望まない場合は初期化する）。';
+$lang['acl_all_templates'] = 'テンプレート対する @ALL グループのアクセス権限（<code>data/pages...</code>に置いた場合）';
+$lang['acl_all_templates_o_0'] = '無し';
+$lang['acl_all_templates_o_1'] = '読取（デフォルト）';
+$lang['acl_all_templates_o_2'] = '編集';
+// $lang['acl_all_templates_o_noacl'] = 'No automatic ACL';
+$lang['acl_user_templates'] = 'テンプレート対する @user グループのアクセス権限（<code>data/pages...</code>に置いた場合）';
+$lang['acl_user_templates_o_0'] = '無し';
+$lang['acl_user_templates_o_1'] = '読取（デフォルト）';
+$lang['acl_user_templates_o_2'] = '編集';
+// $lang['acl_user_templates_o_noacl'] = 'No automatic ACL';
