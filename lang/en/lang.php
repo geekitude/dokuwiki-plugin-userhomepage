549b2cda47a60268a6bb42a009b9bf7662c34129
Simon DELAGE <simon.geekitude@gmail.com>
2014-10-23 23:19:06 +0200

    Added namespaced topbar

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * English language file for Namespaced template
+ * English language file for Userhomepage plugin
  * @author     Simon DELAGE <simon.geekitude@gmail.com>
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
