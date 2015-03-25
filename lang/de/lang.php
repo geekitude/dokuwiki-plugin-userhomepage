ba8739a2b52316541953c6d2fe11b8060f9ae855
Simon DELAGE <simon.geekitude@gmail.com>
2015-03-25 22:50:05 +0100

    Added German translation (B.Molitor contribution)

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * German language file for Userhomepage plugin
+ * @author     Simon DELAGE <simon.geekitude@gmail.com>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+    $lang['copyerror'] = 'Fehler beim Kopieren';
+    $lang['copysuccess'] = 'Erfolgreich kopiert';
+    $lang['createdprivatens'] = 'Privaten Namespace erstellt';
+    $lang['createdpublicpage'] = 'Öffentliche Seite erstellt';
+    $lang['privatenamespace'] = 'Privater Namespace';
+    $lang['publicpage'] = 'Öffentliche Seite';
---
+++ lang/de/settings.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * German settings file for Userhomepage plugin
+ * Previous authors: James GuanFeng Lin, Mikhail I. Izmestev, Daniel Stonier
+ * @author: Simon Delage <simon.geekitude@gmail.com>
+ * @license: CC Attribution-Share Alike 3.0 Unported <http://creativecommons.org/licenses/by-sa/3.0/>
+ */
+
+    $lang['create_private_ns'] = 'Privaten Namespace für die Benutzer anlegen (alle anderen Optionen überprüfen, bevor diese Option aktiviert wird!)';
+    $lang['use_name_string'] = 'Den vollen Namen des Benutzers statt des Logins für seinen privaten Namespace benutzen';
+    $lang['use_start_page'] = 'Den Startseitennamen des Wikis für die Startseiten der privaten Namespaces benutzen (andernfalls wird der Name des privaten Namespace benutzt).';
+    $lang['users_namespace'] = 'Namespace, unter dem die privaten Namespaces der Benutzer angelegt werden';
+    $lang['group_by_name'] = 'Namespaces der Benutzer nach dem ersten Buchstaben des Benutzernamens gruppieren';
+    $lang['edit_before_create'] = 'Benutzern erlauben, die Startseite ihres privaten Namespace beim Anlegen zu bearbeiten (funktioniert nur, wenn nicht gleichzeitig eine öffentliche Seite angelegt wird)';
+    $lang['acl_all_private'] = 'Berechtigungen der Gruppe @ALL für private Namespaces';
+    $lang['acl_all_private_o_0'] = 'Keine (Standard)';
+    $lang['acl_all_private_o_1'] = 'Lesen';
+    $lang['acl_all_private_o_2'] = 'Bearbeiten';
+    $lang['acl_all_private_o_4'] = 'Anlegen';
+    $lang['acl_all_private_o_8'] = 'Hochladen';
+    $lang['acl_all_private_o_16'] = 'Entfernen';
+    $lang['acl_all_private_o_noacl'] = 'Kein automatischer ACL-Eintrag';
+    $lang['acl_user_private'] = 'Berechtigungen der Gruppe @user für private Namespaces';
+    $lang['acl_user_private_o_0'] = 'Keine (Standard)';
+    $lang['acl_user_private_o_1'] = 'Lesen';
+    $lang['acl_user_private_o_2'] = 'Bearbeiten';
+    $lang['acl_user_private_o_4'] = 'Anlegen';
+    $lang['acl_user_private_o_8'] = 'Hochladen';
+    $lang['acl_user_private_o_16'] = 'Entfernen';
+    $lang['acl_user_private_o_noacl'] = 'Kein automatischer ACL-Eintrag';
+    $lang['create_public_page'] = 'Öffentliche Seite für die Benutzer anlegen';
+    $lang['public_pages_ns'] = 'Namespace, unter dem die öffentlichen Seiten der Benutzer angelegt werden';
+    $lang['acl_all_public'] = 'Berechtigungen der Gruppe @ALL für öffentliche Seiten';
+    $lang['acl_all_public_o_0'] = 'Keine';
+    $lang['acl_all_public_o_1'] = 'Read (Default)';
+    $lang['acl_all_public_o_2'] = 'Edit';
+    $lang['acl_all_public_o_noacl'] = 'No automatic ACL';
+    $lang['acl_user_public'] = 'Permissions for @user group on Public Pages';
+    $lang['acl_user_public_o_0'] = 'None';
+    $lang['acl_user_public_o_1'] = 'Lesen (Standard)';
+    $lang['acl_user_public_o_2'] = 'Bearbeiten';
+    $lang['acl_user_public_o_noacl'] = 'Kein automatischer ACL-Eintrag';
+    $lang['templates_path'] = 'Pfad der Templates (userhomepage_private.txt and userhomepage_public.txt), z.B. <code>data/pages/user</code> oder <code>lib/plugins/userhomepage</code>';
+    $lang['templatepath'] = 'Templatepfad aus Version 3.0.4. Exisitert diese Datei, wird sie als Template für die Startseite neuer privater Namespaces verwendet (löschen, wenn dies nicht gewünscht wird)';
+    $lang['acl_all_templates'] = 'Berechtigungen der Gruppe @ALL für Templates, die in <code>data/pages...</code> liegen';
+    $lang['acl_all_templates_o_0'] = 'Keine';
+    $lang['acl_all_templates_o_1'] = 'Lesen (Standard)';
+    $lang['acl_all_templates_o_2'] = 'Bearbeiten';
+    $lang['acl_all_templates_o_noacl'] = 'Kein automatischer ACL-Eintrag';
+    $lang['acl_user_templates'] = 'Berechtigungen der Gruppe @user für Templates, die in <code>data/pages...</code> liegen';
+    $lang['acl_user_templates_o_0'] = 'Keine';
+    $lang['acl_user_templates_o_1'] = 'Lesen (Standard)';
+    $lang['acl_user_templates_o_2'] = 'Bearbeiten';
+    $lang['acl_user_templates_o_noacl'] = 'Kein automatischer ACL-Eintrag';
+    $lang['no_acl'] = 'Automatische Erstellung von ACL-Einträgen deaktivieren; bereits erstellte Einträge müssen manuell entfernt werden. Vergessen Sie nicht, ggf. ACL-Einträge für die Templates zu erstellen, falls diese in <code>data/pages...</code> liegen.';
