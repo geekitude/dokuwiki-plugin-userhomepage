8427a1e5eebf60a21bfc09d6f4287c46fe1205d4
Impeck <impeck@ya.ru>
2025-04-21 07:15:16 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -3,11 +3,20 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Impeck <impeck@ya.ru>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['copyerror']             = 'Ошибка копирования';
 $lang['copysuccess']           = 'Скопировано успешно';
+$lang['copynotneeded']         = 'Копия не требуется';
+$lang['createdprivatens']      = 'Создан ваш приватный namespace';
+$lang['createdpublicpage']     = 'Создан ваш публичный namespace';
 $lang['privatenamespace']      = 'Личное пространство';
 $lang['publicpage']            = 'Публичная страница';
 $lang['publicnamespace']       = 'Публичное пространство';
 $lang['uhpcreated']            = 'Автоматически создано плагином User Homepage';
+$lang['fromskeleton']          = 'Добавлена страница из шаблона User Homepage в ваш приватный namespace';
+$lang['aclupdate']             = 'Добавлены или обновлены правила ACL:';
+$lang['mkdirfailure']          = 'Не удалось создать директорию:';
+$lang['settingsconflict']      = 'Конфликт настроек UserHomePage! Убедитесь, что приватный и публичный namespace различаются. Плагин не будет работать, пока это не исправлено.';
+$lang['avatarsconflict']       = 'Конфликт настроек UserHomePage и плагина Avatar! Убедитесь, что приватный namespace и аватары хранятся в разных местах (иначе правила ACL UserHomePage могут блокировать аватары).';
